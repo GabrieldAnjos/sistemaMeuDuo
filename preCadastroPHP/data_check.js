6fedb3e0c0ce4958e36f8184c36e1fc8ee0ddfd1
Pierre Alvim <alvimpierre@outlook.com>
2020-06-08 09:08:34 -0300

    bloqueio btn, pre-check de dados, limpeza css

--- preCadastroPHP/data_check.js
+++ preCadastroPHP/data_check.js
@@ -11,18 +11,71 @@ function is_email_valid(email) {
 
 $(document).ready(function () {
 
-    //campo de email
+    //campos
     const email_field = $('form > #email')
+    const nome_field = $('form > #nome')
+    const idade_field = $('form > #idade')
+    //btn enviar
+    const btn_enviar = $('#btn_enviar')
 
-    //mostra dica visual que o email está no formato errado
+    /**
+     * Abilitar e Desabilitar o botão de envio de acordo com a validade dos campos
+     */
+    function btn_change() {
+        if (email_field.is_valid && nome_field.is_valid && nome_field.is_valid) {
+            btn_enviar.attr('disabled', false)
+            btn_enviar.removeClass('curtaindown_blocked')
+            btn_enviar.addClass('curtaindown')
+        }
+        else {
+            btn_enviar.attr('disabled', true)
+            btn_enviar.removeClass('curtaindown')
+            btn_enviar.addClass('curtaindown_blocked')
+        }
+    }
+
+
+    //mostra dica visual que o email está no formato errado e desabilida o envio
     email_field.on('input', () => {
 
-        if (is_email_valid(email_field.val()))
-            email_field.removeClass('error');
-        else
-            email_field.addClass('error');
-
+        if (is_email_valid(email_field.val())) {
+            email_field.removeClass('error')
+            email_field.is_valid = true
+        }
+        else {
+            email_field.addClass('error')
+            email_field.is_valid = false
+        }
+        btn_change()
     })
 
-});
+    //mostra dica visual se o nome está vazio e desabilida o envio
+    nome_field.on('input', () => {
+        if (nome_field.val() != "") {
+            nome_field.removeClass('error')
+            nome_field.is_valid = true
+        }
+        else {
+            nome_field.addClass('error')
+            nome_field.is_valid = false
+        }
+        btn_change()
+    })
 
+    //mostra dica visual se a idade está entre 5 e 99 anos e desabilida o envio
+    idade_field.on('input', () => {
+
+        if (parseInt(idade_field.val()) > 5 && parseInt(idade_field.val()) < 99) {
+            idade_field.removeClass('error')
+            idade_field.is_valid = true
+        }
+        else {
+            idade_field.addClass('error')
+            idade_field.is_valid = false
+        }
+        btn_change()
+    })
+
+
+
+});
--- preCadastroPHP/index.php
+++ preCadastroPHP/index.php
@@ -2,28 +2,14 @@
 
 <html>
 
-    
-
 <head>
-
-
-
-    <!--Let browser know website is optimized for mobile-->
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    
-    <script src="data_check.js"></script>
-
-    <link href="css/PreCadastro.css" rel="stylesheet">
-
-    <link rel="stylesheet" type="text/css" href="css/index.css">
-
     <meta charset="UTF-8">
-
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@515&family=Ubuntu:wght@300;400&display=swap" rel="stylesheet">
-
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="data_check.js"></script>
 </head>
 
 
@@ -33,220 +19,220 @@
 
 
 
-<div class="preCadastro-container">
-    <section class="esquerda">
+    <div class="preCadastro-container">
+        <section class="esquerda">
 
-        <h1>Concorra a prêmios no dia do lançamento</h1>
+            <h1>Concorra a prêmios no dia do lançamento</h1>
 
-        <form method="post" action="salvar.php">
+            <form method="post" action="salvar.php">
 
-            <input name="nome" id="nome" type="text" placeholder="Nome" />
+                <input name="nome" id="nome" type="text" placeholder="Nome" />
 
-            <input name="email" id="email" type="text" placeholder="E-mail (Só aviso do lançamento)" />
+                <input name="email" id="email" type="text" placeholder="E-mail (Só aviso do lançamento)" />
 
-            <input name="idade" id="idade" type="text" placeholder="Idade" />
+                <input name="idade" id="idade" type="number" min="5" max="100" placeholder="Idade" />
 
-            <h4>Quantas estrelas vc daria pelo seu motivo de usar o Meu Duo? Para encontrar alguém que...</h4>
+                <h4>Quantas estrelas vc daria pelo seu motivo de usar o Meu Duo? Para encontrar alguém que...</h4>
 
-            <div id="afirmacoes">
+                <div id="afirmacoes">
 
-                <p>Perdoe suas noobagens assim como você perdoa as dela</p>
+                    <p>Perdoe suas noobagens assim como você perdoa as dela</p>
 
-                <section id="pergunta1" class="rating">
+                    <section id="pergunta1" class="rating">
 
-                    <input type="radio" id="sstar_5" name="pergunta1" value="5" />
+                        <input type="radio" id="sstar_5" name="pergunta1" value="5" />
 
-                    <label for="sstar_5" >&#9733;</label>
+                        <label for="sstar_5">&#9733;</label>
 
 
 
-                    <input type="radio" id="sstar_4" name="pergunta1" value="4" />
+                        <input type="radio" id="sstar_4" name="pergunta1" value="4" />
 
-                    <label for="sstar_4" >&#9733;</label>
+                        <label for="sstar_4">&#9733;</label>
 
 
 
-                    <input type="radio" id="sstar_3" name="pergunta1" value="3" />
+                        <input type="radio" id="sstar_3" name="pergunta1" value="3" />
 
-                    <label for="sstar_3" >&#9733;</label>
+                        <label for="sstar_3">&#9733;</label>
 
 
 
-                    <input type="radio" id="sstar_2" name="pergunta1" value="2" />
+                        <input type="radio" id="sstar_2" name="pergunta1" value="2" />
 
-                    <label for="sstar_2" >&#9733;</label>
+                        <label for="sstar_2">&#9733;</label>
 
 
 
-                    <input type="radio" id="sstar_1" name="pergunta1" value="1" />
+                        <input type="radio" id="sstar_1" name="pergunta1" value="1" />
 
-                    <label for="sstar_1" >&#9733;</label>
+                        <label for="sstar_1">&#9733;</label>
 
-                </section>
+                    </section>
 
 
 
-                <p>Tenha aquele plano infalível para darem gg aos 20min</p>
+                    <p>Tenha aquele plano infalível para darem gg aos 20min</p>
 
-                <section id="pergunta2" class="rating">
+                    <section id="pergunta2" class="rating">
 
 
 
-                    <input type="radio" id="sttar_5" name="pergunta2" value="5" />
+                        <input type="radio" id="sttar_5" name="pergunta2" value="5" />
 
-                    <label for="sttar_5" >&#9733;</label>
+                        <label for="sttar_5">&#9733;</label>
 
 
 
-                    <input type="radio" id="sttar_4" name="pergunta2" value="4" />
+                        <input type="radio" id="sttar_4" name="pergunta2" value="4" />
 
-                    <label for="sttar_4" >&#9733;</label>
+                        <label for="sttar_4">&#9733;</label>
 
 
 
-                    <input type="radio" id="sttar_3" name="pergunta2" value="3" />
+                        <input type="radio" id="sttar_3" name="pergunta2" value="3" />
 
-                    <label for="sttar_3" >&#9733;</label>
+                        <label for="sttar_3">&#9733;</label>
 
 
 
-                    <input type="radio" id="sttar_2" name="pergunta2" value="2" />
+                        <input type="radio" id="sttar_2" name="pergunta2" value="2" />
 
-                    <label for="sttar_2" >&#9733;</label>
+                        <label for="sttar_2">&#9733;</label>
 
 
 
-                    <input type="radio" id="sttar_1" name="pergunta2" value="1" />
+                        <input type="radio" id="sttar_1" name="pergunta2" value="1" />
 
-                    <label for="sttar_1" >&#9733;</label>
+                        <label for="sttar_1">&#9733;</label>
 
-                </section>
+                    </section>
 
 
 
-                <p>De aquele belo trato  ͡° ͜ʖ ͡° na sua skin</p>
-                
+                    <p>De aquele belo trato ͡° ͜ʖ ͡° na sua skin</p>
 
-                <section id="pergunta3" class="rating">
 
+                    <section id="pergunta3" class="rating">
 
 
-                    <input type="radio" id="staar_5" name="pergunta3" value="5" />
 
-                    <label for="staar_5" >&#9733;</label>
+                        <input type="radio" id="staar_5" name="pergunta3" value="5" />
 
+                        <label for="staar_5">&#9733;</label>
 
 
-                    <input type="radio" id="staar_4" name="pergunta3" value="4" />
 
-                    <label for="staar_4" >&#9733;</label>
+                        <input type="radio" id="staar_4" name="pergunta3" value="4" />
 
+                        <label for="staar_4">&#9733;</label>
 
 
-                    <input type="radio" id="staar_3" name="pergunta3" value="3" />
 
-                    <label for="staar_3" >&#9733;</label>
+                        <input type="radio" id="staar_3" name="pergunta3" value="3" />
 
+                        <label for="staar_3">&#9733;</label>
 
 
-                    <input type="radio" id="staar_2" name="pergunta3" value="2" />
 
-                    <label for="staar_2" >&#9733;</label>
+                        <input type="radio" id="staar_2" name="pergunta3" value="2" />
 
+                        <label for="staar_2">&#9733;</label>
 
 
-                    <input type="radio" id="staar_1" name="pergunta3" value="1" />
 
-                    <label for="staar_1" >&#9733;</label>
+                        <input type="radio" id="staar_1" name="pergunta3" value="1" />
 
-                </section>
+                        <label for="staar_1">&#9733;</label>
 
+                    </section>
 
 
-                <p>Te acompanhe naquela dive suicida spamando maestria igual maluco</p>
 
-                <section id="pergunta4" class="rating">
+                    <p>Te acompanhe naquela dive suicida spamando maestria igual maluco</p>
 
+                    <section id="pergunta4" class="rating">
 
 
-                    <input type="radio" id="starr_5" name="pergunta4" value="5" />
 
-                    <label for="starr_5" >&#9733;</label>
+                        <input type="radio" id="starr_5" name="pergunta4" value="5" />
 
+                        <label for="starr_5">&#9733;</label>
 
 
-                    <input type="radio" id="starr_4" name="pergunta4" value="4" />
 
-                    <label for="starr_4" >&#9733;</label>
+                        <input type="radio" id="starr_4" name="pergunta4" value="4" />
 
+                        <label for="starr_4">&#9733;</label>
 
 
-                    <input type="radio" id="starr_3" name="pergunta4" value="3" />
 
-                    <label for="starr_3" >&#9733;</label>
+                        <input type="radio" id="starr_3" name="pergunta4" value="3" />
 
+                        <label for="starr_3">&#9733;</label>
 
 
-                    <input type="radio" id="starr_2" name="pergunta4" value="2" />
 
-                    <label for="starr_2" >&#9733;</label>
+                        <input type="radio" id="starr_2" name="pergunta4" value="2" />
 
+                        <label for="starr_2">&#9733;</label>
 
 
-                    <input type="radio" id="starr_1" name="pergunta4" value="1" />
 
-                    <label for="starr_1" >&#9733;</label>
+                        <input type="radio" id="starr_1" name="pergunta4" value="1" />
 
-                </section>
+                        <label for="starr_1">&#9733;</label>
 
-            </div>
+                    </section>
 
+                </div>
 
 
-            <textarea name="sugestao" id="sugestao" cols="80" rows="10" placeholder="Tem alguma ideia ou sugestão?                                   Fala pra gente, somos todo ouvidos :)"></textarea>
 
-            <button class="curtaindown" type="submit" name="btn_salvar">Enviar</button>
+                <textarea name="sugestao" id="sugestao" cols="80" rows="10" placeholder="Tem alguma ideia ou sugestão?                                   Fala pra gente, somos todo ouvidos :)"></textarea>
 
-        </form>
+                <button disabled="true" id="btn_enviar" class="curtaindown_blocked" type="submit" name="btn_salvar">Enviar</button>
 
-    </section>
+            </form>
 
-    <section class="direita">
+        </section>
 
-        <header>
+        <section class="direita">
 
-            <img id="intro" src="assets/introLink.svg" alt="" />
+            <header>
 
-        </header>
+                <img id="intro" src="assets/introLink.svg" alt="" />
 
-        <main>
+            </header>
 
-            <img id="logo" src="assets/logoLink.svg" alt="" />
+            <main>
 
-        </main>
+                <img id="logo" src="assets/logoLink.svg" alt="" />
 
-        <footer>
+            </main>
 
-            <div id="parceiros">
+            <footer>
 
-                <h1>Parceiros</h1>
+                <div id="parceiros">
 
-                <a href="https://www.youtube.com/playeronegames"> <img title="PlayerOneGames" src="assets/parceiro1.svg" alt="PlayerOneGames"></a>
+                    <h1>Parceiros</h1>
 
-                <a href="https://www.facebook.com/groups/IDFOficial/"> <img title="IlhaDeFreljord"  src="assets/parceiro2.svg" alt="IlhaDeFreljord"></a>
+                    <a href="https://www.youtube.com/playeronegames"> <img title="PlayerOneGames" src="assets/parceiro1.svg" alt="PlayerOneGames"></a>
 
-            </div>
+                    <a href="https://www.facebook.com/groups/IDFOficial/"> <img title="IlhaDeFreljord" src="assets/parceiro2.svg" alt="IlhaDeFreljord"></a>
 
-        </footer>
+                </div>
 
-    </section>
+            </footer>
 
-    
+        </section>
 
-</div>
 
-        
 
-	</body>
+    </div>
+
+
+
+</body>
 
 </html>
