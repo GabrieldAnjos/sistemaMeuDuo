15ab34cd164929b0de997ae53a6b421f1e9ac9f1
Pierre Alvim <alvimpierre@outlook.com>
2020-07-03 19:08:07 -0300

    Riot Api no frontend

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Login from './pages/Login';
-import Main from './pages/Main';
+import Login from './pages/Login_user';
+import Main from './pages/Main_user';
 
 export default function Routes() {
     return(
         <BrowserRouter>
             <Route path="/" exact component={Login} />
-            <Route path="/dev/:id" component={Main} />
+            <Route path="/user/:id" component={Main} />
         </BrowserRouter>
     );
 }
